530d4f3c5d23d5c1fb1217b5a93661fc87989e59
Mats Rauhala <mats.rauhala@gmail.com>
2011-07-07 23:45:50 +0300

    Dependency on krumo.
    Show page

--- handler.php
+++ handler.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once("class.krumo.php"); // Dependency on krumo
+
 function convertArg($a)
 {
     if(is_callable($a))
@@ -32,14 +34,57 @@ function _yaml_emit_file($file, $data)
     file_put_contents($file, yaml_emit($data));
 }
 
+function coalesce(&$x, $def = null)
+{
+    if(!isset($x))
+        return $def;
+    return $x;
+}
+
+function escapeInput($args)
+{
+    return array_map(function($x) {
+        $y = htmlspecialchars($x);
+        return "'$y' (" . (($x == $y) ? "original" : "escaped") . ")";
+    }, $args);
+}
+
+function errorPage($id, $info)
+{
+    $bt = array();
+    foreach($info["backtrace"] as $stack)
+    {
+        $fn = $stack["function"];
+        $bt[$fn] = $stack;
+    }
+    $info["backtrace"] = $bt;
+?>
+<html>
+    <head>
+    </head>
+    </body>
+    <h2>Internal server error</h2>
+    <p>Information about the error has been saved in (<?php echo $id ?>)</p>
+        <?php echo krumo($info); ?>
+    </body>
+</html>
+<?php
+}
+
 set_error_handler(function($errno, $errstr) {
     $bt = array_map('convertArgs', array_splice(debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT), 1));
     $time = time();
-    $yaml = md5($time . $errstr . combineFunctionNames($bt)) . ".yaml";
-    echo _yaml_emit_file($yaml, array(
+    $id = md5($time . $errstr . combineFunctionNames($bt));
+    $yaml = $id . ".yaml";
+    $info = array(
         "time" => $time,
         "msg" => $errstr,
         "errno" => $errno,
-        "backtrace" => $bt));
+        "post" => escapeInput($_POST),
+        "get" => escapeInput($_GET),
+        "uri" => coalesce($_SERVER["REQUEST_URI"]),
+        "backtrace" => $bt);
+    _yaml_emit_file($yaml, $info);
+    errorPage($id, $info);
     return true;
 });
